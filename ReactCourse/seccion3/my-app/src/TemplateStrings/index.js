7bcb24183ae28cf2237597e10a08f7f6e9cebaa0
Mateo Lomas <mateo.lomas@yachaytech.edu.ec>
2022-02-11 09:28:24 -0500

    Template Strings

---
+++ ReactCourse/seccion3/my-app/src/TemplateStrings/index.js
@@ -0,0 +1,10 @@
+
+
+const firstThing = 'Hello';
+const secondThing = 'World';
+
+function getMessage(name) {
+  return firstThing + ' ' + secondThing + ' to ' + name ;
+}
+
+console.log(`${getMessage('Mateo')}`);
